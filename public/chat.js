4d25a37bba80a57399bf0ed88f5c6d8e5024bcc7
Michael Lei <michaelleie0ca@gmail.com>
2023-05-14 12:47:41 -0700

    PubNub chat

--- public/chat.js
+++ public/chat.js
@@ -1,25 +1,24 @@
 let chat = document.getElementById("chatForm");
 let chatInput = document.getElementById("chatInput");
-let sender = document.getElementById("sender");
+let msgSender = document.getElementById("sender");
 let messageContainer = document.getElementById("messageContainer");
+let feedback = document.getElementById("feedback");
 
 pubnub.subscribe({
   channels: ["chatroom-1"],
 });
 
-// pubnub.fetchMessages(
-//   {
-//     channels: ["chatroom-1"],
-//     count: 5,
-//   },
-//   function (status, response) {
-//     // handle status, response
-//     console.log(response);
-//     response.channels["chatroom-1"].forEach((message) => {
-//       createMessage(message.message.text, message.message.sender);
-//     });
-//   }
-// );
+pubnub.fetchMessages(
+  {
+    channels: ["chatroom-1"],
+    count: 5,
+  },
+  function (status, response) {
+    response.channels["chatroom-1"].forEach(({ message }) => {
+      createMessage(message);
+    });
+  }
+);
 
 // add listener
 const listener = {
@@ -31,25 +30,48 @@ const listener = {
   message: ({ message }) => {
     createMessage(message);
   },
+  signal: ({ message }) => {
+    if (message.sender === msgSender.value) return;
+    if (message.typing) {
+      feedback.innerHTML = `<p><em>${message.sender} is typing a message...</em></p>`;
+    } else {
+      feedback.innerHTML = "";
+    }
+  },
 };
 pubnub.addListener(listener);
 
 const createMessage = ({ text, sender, timestamp }) => {
-  const messageElement = `
-    <div class="message">
-    <div class="message-info">
-    <div class="message-sender">${sender}</div>
-    <div class="message-time">${new Date(timestamp).toLocaleTimeString(
-      "en-US",
-      {
-        hour: "numeric",
-        minute: "2-digit",
-      }
-    )}</div>
-        </div>
-        <div class="message-text">${text}</div>
-    </div>
- `;
+  const messageElement =
+    sender !== msgSender.value
+      ? `
+      <div class="message message-right">
+          <div class="message-text">${text}</div>
+          <div class="message-info">
+      <div class="message-sender">${sender}</div>
+      <div class="message-time">${new Date(timestamp).toLocaleTimeString(
+        "en-US",
+        {
+          hour: "numeric",
+          minute: "2-digit",
+        }
+      )}</div>
+          </div>
+      </div>
+  `
+      : ` <div class="message">
+      <div class="message-info">
+      <div class="message-sender">${sender}</div>
+      <div class="message-time">${new Date(timestamp).toLocaleTimeString(
+        "en-US",
+        {
+          hour: "numeric",
+          minute: "2-digit",
+        }
+      )}</div>
+          </div>
+          <div class="message-text sender">${text}</div>
+      </div>`;
   messageContainer.innerHTML += messageElement;
 };
 
@@ -60,7 +82,7 @@ const publishMessage = async (message) => {
     channel: "chatroom-1",
     message: {
       text: message,
-      sender: sender.value,
+      sender: msgSender.value,
       timestamp: Date.now(),
     },
   };
@@ -70,6 +92,57 @@ const publishMessage = async (message) => {
 chat.addEventListener("submit", async (e) => {
   e.preventDefault();
   let message = chatInput.value;
+  await pubnub.signal({
+    channel: "chatroom-1",
+    message: {
+      sender: msgSender.value,
+      typing: false,
+    },
+  });
   await publishMessage(message);
-  document.getElementById("chatInput").value = "";
+  chatInput.value = "";
+});
+
+let isTyping = false; // Track the typing state
+let typingTimeout; // Store the reference to the timeout
+
+chatInput.addEventListener("keydown", async (e) => {
+  if (e.key === "Backspace" || e.key === "Delete") {
+    // User removed the message they were typing
+    clearTimeout(typingTimeout);
+    isTyping = false;
+
+    await pubnub.signal({
+      channel: "chatroom-1",
+      message: {
+        sender: msgSender.value,
+        typing: false,
+      },
+    });
+  } else {
+    // User is typing a message
+    if (!isTyping) {
+      await pubnub.signal({
+        channel: "chatroom-1",
+        message: {
+          sender: msgSender.value,
+          typing: true,
+        },
+      });
+    }
+
+    isTyping = true;
+
+    clearTimeout(typingTimeout);
+    typingTimeout = setTimeout(async () => {
+      await pubnub.signal({
+        channel: "chatroom-1",
+        message: {
+          sender: msgSender.value,
+          typing: false,
+        },
+      });
+      isTyping = false;
+    }, 1000);
+  }
 });
